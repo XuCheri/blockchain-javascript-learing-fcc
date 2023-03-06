df7aa65bb8f714c7bd9e7319c6b86f57ee7c16cf
cheri <1156429007@qq.com>
2023-03-07 01:42:42 +0800

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,7 @@
  * @Author: cheri 1156429007@qq.com
  * @Date: 2023-03-06 19:21:06
  * @LastEditors: cheri 1156429007@qq.com
- * @LastEditTime: 2023-03-07 01:29:29
+ * @LastEditTime: 2023-03-07 01:35:15
  * @FilePath: /blockchain-javascript-learing-fcc/hardhat.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
