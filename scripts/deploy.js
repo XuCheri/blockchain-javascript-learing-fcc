aceee74b4a12a38a9d86a18124817364af8914ec
cheri <1156429007@qq.com>
2023-03-07 00:10:16 +0800

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,7 +2,7 @@
  * @Author: cheri 1156429007@qq.com
  * @Date: 2023-03-06 19:21:06
  * @LastEditors: cheri 1156429007@qq.com
- * @LastEditTime: 2023-03-06 22:43:15
+ * @LastEditTime: 2023-03-07 00:04:54
  * @FilePath: /blockchain-javascript-learing-fcc/scripts/deploy.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -20,6 +20,13 @@ async function main() {
         await simpleStorage.deployTransaction.wait(6)
         await verify(simpleStorage.address, [])
     }
+    const currentValue = await simpleStorage.retrieve()
+    console.log(`Current value is ${currentValue}`)
+
+    const transactionResponse = await simpleStorage.store(6)
+    await transactionResponse.wait(1)
+    const updatedValue = await simpleStorage.retrieve()
+    console.log(`Updated value is ${updatedValue}`)
 }
 
 async function verify(contractAddress, args) {
