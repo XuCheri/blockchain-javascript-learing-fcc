f46cd130908afee781dffe75088c6b47d806a696
cheri <1156429007@qq.com>
2023-03-07 00:32:59 +0800

    custom hardhat  learn hardhat node/console

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,13 +2,14 @@
  * @Author: cheri 1156429007@qq.com
  * @Date: 2023-03-06 19:21:06
  * @LastEditors: cheri 1156429007@qq.com
- * @LastEditTime: 2023-03-06 21:38:42
+ * @LastEditTime: 2023-03-07 00:26:44
  * @FilePath: /blockchain-javascript-learing-fcc/hardhat.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 require("@nomicfoundation/hardhat-toolbox")
 require("dotenv").config()
 require("@nomiclabs/hardhat-etherscan")
+require("./tasks/block-number")
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
@@ -23,6 +24,10 @@ module.exports = {
             url: SEPOLIA_RPC_URL,
             accounts: [PRIVATE_KEY],
             chainId: 11155111,
+        },
+        localhost: {
+            url: "http://127.0.0.1:8545/",
+            chainId: 31337,
         },
     },
     etherscan: {
