35e0399c3abbe3a738a21d0ec33d8a0bd6f06ad4
cheri <1156429007@qq.com>
2023-03-07 22:08:43 +0800

    complete basic works

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,46 +1,62 @@
-/*
- * @Author: cheri 1156429007@qq.com
- * @Date: 2023-03-06 19:21:06
- * @LastEditors: cheri 1156429007@qq.com
- * @LastEditTime: 2023-03-07 01:35:15
- * @FilePath: /blockchain-javascript-learing-fcc/hardhat.config.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
-require("@nomicfoundation/hardhat-toolbox")
-require("dotenv").config()
-require("@nomiclabs/hardhat-etherscan")
-require("./tasks/block-number")
+require("@nomiclabs/hardhat-waffle")
 require("hardhat-gas-reporter")
+require("@nomiclabs/hardhat-etherscan")
+require("dotenv").config()
 require("solidity-coverage")
+require("hardhat-deploy")
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 
-const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+const Sepolia_RPC_URL = process.env.Sepolia_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
-const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-    solidity: "0.8.8",
     defaultNetwork: "hardhat",
     networks: {
-        Sepolia: {
-            url: SEPOLIA_RPC_URL,
+        hardhat: {
+            chainId: 31337,
+            // gasPrice: 130000000000,
+        },
+        sepolia: {
+            url: Sepolia_RPC_URL,
             accounts: [PRIVATE_KEY],
             chainId: 11155111,
+            blockConfirmations: 6,
         },
-        localhost: {
-            url: "http://127.0.0.1:8545/",
-            chainId: 31337,
-        },
+    },
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.7",
+            },
+            {
+                version: "0.6.6",
+            },
+        ],
     },
     etherscan: {
         apiKey: ETHERSCAN_API_KEY,
+        // customChains: [], // uncomment this line if you are getting a TypeError: customChains is not iterable
     },
     gasReporter: {
-        enabled: false,
+        enabled: true,
+        currency: "USD",
         outputFile: "gas-report.txt",
         noColors: true,
-        currency: "USD",
-        coinmarketcap: COINMARKETCAP_API_KEY,
+        // coinmarketcap: COINMARKETCAP_API_KEY,
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0, // here this will by default take the first account as deployer
+            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+        },
+    },
+    mocha: {
+        timeout: 500000,
     },
 }
