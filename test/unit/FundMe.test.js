0dd92bc7689d24b8b6912536c9ea866944ec9918
cheri <1156429007@qq.com>
2023-03-11 00:59:17 +0800

    test demo

---
+++ test/unit/FundMe.test.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: cheri 1156429007@qq.com
+ * @Date: 2023-03-11 00:48:23
+ * @LastEditors: cheri 1156429007@qq.com
+ * @LastEditTime: 2023-03-11 00:56:35
+ * @FilePath: /blockchain-javascript-learing-fcc/test/unit/FundMe.test.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+describe("FundMe", async function () {
+    beforeEach()
+})
