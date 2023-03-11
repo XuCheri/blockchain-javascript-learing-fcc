d1526214548a611fcf8ba9027eda33ecb1bc3143
cheri <1156429007@qq.com>
2023-03-11 20:09:41 +0800

    staging test

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,7 @@
  * @Author: cheri 1156429007@qq.com
  * @Date: 2023-03-08 15:05:43
  * @LastEditors: cheri 1156429007@qq.com
- * @LastEditTime: 2023-03-11 15:45:37
+ * @LastEditTime: 2023-03-11 19:39:11
  * @FilePath: /blockchain-javascript-learing-fcc/hardhat.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -49,7 +49,7 @@ module.exports = {
         // customChains: [], // uncomment this line if you are getting a TypeError: customChains is not iterable
     },
     gasReporter: {
-        enabled: true,
+        enabled: false,
         currency: "USD",
         outputFile: "gas-report.txt",
         noColors: true,
---
+++ scripts/fund.js
@@ -0,0 +1,19 @@
+/*
+ * @Author: cheri 1156429007@qq.com
+ * @Date: 2023-03-11 19:37:56
+ * @LastEditors: cheri 1156429007@qq.com
+ * @LastEditTime: 2023-03-11 19:42:19
+ * @FilePath: /blockchain-javascript-learing-fcc/scripts/fund.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+const { getNamedAccounts } = require("hardhat")
+
+async function main() {}
+main()
+    .then(() => {
+        process.exit(0)
+    })
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
