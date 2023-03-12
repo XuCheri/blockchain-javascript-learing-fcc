3b6e979bbf0153afc593e57e51e23224f166d614
cheri <1156429007@qq.com>
2023-03-13 01:29:12 +0800

    Create demo.js

---
+++ frontend/demo.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: cheri 1156429007@qq.com
+ * @Date: 2023-03-13 01:28:51
+ * @LastEditors: cheri 1156429007@qq.com
+ * @LastEditTime: 2023-03-13 01:28:56
+ * @FilePath: /blockchain-javascript-learing-fcc/frontend/demo.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+console.log(demo);
